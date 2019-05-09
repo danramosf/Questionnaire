272890bf2f5d52584018474c7c828c81cd73889d
danramosf <danramosf@gmail.com>
2019-05-09 12:52:55 -0400

    MapAndPersist-NetDriver script function added.

---
+++ Powershell/MapAndPersist-NetDriver.ps1
@@ -0,0 +1,16 @@
+﻿# Author: Daniel Ferreira
+# 2019-05-09
+
+# This function receives a driver letter and a network share path as a parameter
+# and maps and persists a network drive with the specified parameters.
+
+function MapAndPersist-NetDriver
+{
+    Param (
+        [Parameter(Mandatory = $true)][Char] $driver_letter,
+        [Parameter(Mandatory = $true)][String] $netshare_path
+    )
+
+    Write-Host "Driver Letter: $driver_letter , Network Share Path: $netshare_path"
+    New-PSDrive -Name $driver_letter -PSProvider FileSystem -Root $netshare_path -Persist
+}
