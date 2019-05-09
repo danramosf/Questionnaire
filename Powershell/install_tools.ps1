418bbfaa0e2e6a3e10fdca4aaab78ac3272b56dc
danramosf <danramosf@gmail.com>
2019-05-09 00:55:37 -0400

    install_tools.ps1 - Included download and install of .net 4.5

--- Powershell/install_tools.ps1
+++ Powershell/install_tools.ps1
@@ -1,18 +1,31 @@
 #Author: Daniel Ferreira
 # 08-05-2019
 
-# .NET 4.5 download url
-#	$url_45 = "https://download.microsoft.com/download/B/A/4/BA4A7E71-2906-4B2D-A0E1-80CF16844F5F/dotNetFx45_Full_setup.exe"
-# .NET 4.5 file name
-#	$file_45 = "$PSScriptRoot\dotNetFx45_Full_setup.exe"
-
-# Downloading .NET 4.5
-#	(New-Object System.Net.WebClient).DownloadFile($url, $output)
-
-
-
 #Arguments for starting the process of installation
 $args = " /q /norestart"
+
+#Checking if .NET 4.5 is already installed...
+$disp_name_45 = "Microsoft .NET Framework 4.`[56789]`."
+$installed_45 = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where { $_.DisplayName -Match $disp_name_45 }) -ne $null
+
+If(-Not $installed_45){
+
+	# .NET 4.5 download url
+	$url_45 = "https://download.microsoft.com/download/B/A/4/BA4A7E71-2906-4B2D-A0E1-80CF16844F5F/dotNetFx45_Full_setup.exe"
+	# .NET 4.5 file name
+	$file_45 = "$PSScriptRoot\dotNetFx45_Full_setup.exe"
+	
+	Write-Host "Downloading .NET Framework 4.5"
+	# Downloading .NET 4.5
+	(New-Object System.Net.WebClient).DownloadFile($url_45, $file_45)
+	
+	Write-Host "Installing .NET Framework 4.5"
+	#Installing .NET 4.5
+	Start-Process -FilePath $file_45 -ArgumentList $args -Wait
+
+}else{
+	Write-Host ".NET 4.5 Framework or a higher version is already installed."
+}
 
 #Checking if .NET CORE 2.2.203 is already installed...
 $disp_name_core = "Microsoft .NET Core SDK 2.2.203 (x64)"
